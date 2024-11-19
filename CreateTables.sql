d4f7fe6f06b6d41846ac55c1badc3245854ddad9
Cheng Shi <sc971008@gmail.com>
2024-11-19 15:40:53 -0500

    Added script to create tables and importing

---
+++ CreateTables.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS Key_SKU;
+CREATE TABLE Key_SKU(
+	ProductSKU	TEXT PRIMARY KEY,
+	stockCode	INTEGER
+);
+
+DROP TABLE IF EXISTS Online_Transactions;
+CREATE TABLE Online_Transactions(
+	TransactionID INTEGER PRIMARY KEY,
+	TransactionDate date,
+	ProductSKU TEXT NOT NULL,
+	ProductName TEXT,
+	ProductCategory TEXT,
+	Qty INTEGER,
+	AvgPrice REAL,
+	Revenue REAL,
+	Tax REAL,
+	Delivery REAL,
+	FOREIGN KEY(ProductSKU) REFERENCES Key_SKU(ProductSKU)
+);
+
+DROP TABLE IF EXISTS Invoice;
+CREATE TABLE Invoice(
+	InvoiceNo INTEGER,
+	InvoiceDate date,
+	StockCode INTEGER,
+	Qty Integer,
+	PRIMARY KEY(InvoiceNo, InvoiceDate, StockCode),
+	FOREIGN KEY(StockCode) REFERENCES Key_SKU(StockCode)
+);
+
+DROP TABLE IF EXISTS Market_Spending;
+CREATE TABLE Market_Spending(
+	SpendDate date PRIMARY KEY,
+	offlineSpending REAL,
+	onlineSpending REAL
+);
---
+++ SQLCode/ReadCSV.sql
@@ -0,0 +1,4 @@
+.import cleanData/New_Retail.csv Invoice
+.import cleanData/ Market_Spending
+.import cleanData/ Online_Transactions
+.import cleanData/ Key_SKU
